431fa0ec7eeb5b4bb18378bd81cd670956a3dd7c
takahiro6730 <takahiroh042100@gmail.com>
2024-04-29 15:21:13 +0900

    feat: homepage front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import HomePage from "@/components/homepage/page";
+import HomePage from "@/components/homepage/Homepage";
 
 export default function Home() {
   return (
